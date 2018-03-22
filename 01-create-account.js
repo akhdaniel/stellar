4d2be9e3b8988879de99c9cb2aa7e2c4a74ab231
Akhmad Daniel Sembiring <akhmad.daniel@gmail.com>
2018-03-22 11:01:58 +0700

    create accoun4

---
+++ 01-create-account.js
@@ -0,0 +1,41 @@
+/********************************************************
+Stellar create account script
+Akhmad D. Sembiring
+steps:
+npm install --save stellar-sdk
+*********************************************************/
+var StellarSdk = require('stellar-sdk');
+var pair = StellarSdk.Keypair.random();
+
+// generate secret key and public key
+secret = pair.secret();
+console.log("secret: "+secret);
+public= pair.publicKey();
+console.log("public: "+public);
+
+
+// The SDK does not have tools for creating test accounts, so you'll have to
+// make your own HTTP request.
+var request = require('request');
+request.get({
+  url: 'https://friendbot.stellar.org',
+  qs: { addr: pair.publicKey() },
+  json: true
+}, function(error, response, body) {
+  if (error || response.statusCode !== 200) {
+    console.error('ERROR!', error || body);
+  }
+  else {
+    console.log('SUCCESS! You have a new account :)\n', body);
+	var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+	// the JS SDK uses promises for most actions, such as retrieving an account
+	server.loadAccount(pair.publicKey()).then(function(account) {
+  		console.log('Balances for account: ' + pair.publicKey());
+  		account.balances.forEach(function(balance) {
+    	console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+  	});
+	});
+
+  }
+});
+
